8572900d88d0d3c57c6bc1e027ac53de2da5700d
liuxunguang <liuxunguang@pangu16.com>
2019-05-28 14:03:38 +0800

    修改代码

--- src/test/java/com/liuxg/demo/JpaTestApplicationTests.java
+++ src/test/java/com/liuxg/demo/JpaTestApplicationTests.java
@@ -1,9 +1,12 @@
 package com.liuxg.demo;
 
 import com.liuxg.demo.config.HibernateConfig;
+import com.liuxg.demo.repository.ConsumerRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -12,10 +15,16 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class JpaTestApplicationTests {
 
+    @Autowired
+    RedisTemplate redisTemplate;
+
+    @Autowired
+    ConsumerRepository consumerRepository;
 
     @Test
     public void contextLoads() {
-
+        consumerRepository.findAll();
+        redisTemplate.opsForValue().get("xx");
     }
 
 }
